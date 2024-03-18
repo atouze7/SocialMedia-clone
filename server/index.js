a93b83e74eb8f985f77d46cdb5a50b0912d2d0e1
atouze7 <89221133+atouze7@users.noreply.github.com>
2024-03-18 16:01:50 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -24,14 +24,14 @@ const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(cors({
+  origin: 'https://social-media-clone-bay.vercel.app',
+}));
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors({
-  origin: 'https://social-media-clone-bay.vercel.app',
-}));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */

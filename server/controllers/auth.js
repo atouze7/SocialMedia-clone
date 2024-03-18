62f879b232396830c607e546ed67464f906e3726
atouze7 <89221133+atouze7@users.noreply.github.com>
2024-03-18 18:49:27 -0400

    Update auth.js

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -34,7 +34,7 @@ export const register = async (req, res) => {
 
     const savedUser = await newUser.save();
     console.log(savedUser);
-    res.status(201).json(savedUser);
+    res.status(201).json(newUser);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }

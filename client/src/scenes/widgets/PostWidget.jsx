6283b1c23e621d225a1ae7fe3ce36ddc2417428d
atouze7 <89221133+atouze7@users.noreply.github.com>
2024-03-18 14:17:39 -0400

    Update PostWidget.jsx

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -35,7 +35,7 @@ const PostWidget = ({
   const primary = palette.primary.main;
 
   const patchLike = async () => {
-    const response = await fetch(`social-media-clone-api-three.vercel.app/posts/${postId}/like`, {
+    const response = await fetch(`https://social-media-clone-api-three.vercel.app/posts/${postId}/like`, {
       method: "PATCH",
       headers: {
         Authorization: `Bearer ${token}`,
@@ -64,7 +64,7 @@ const PostWidget = ({
           height="auto"
           alt="post"
           style={{ borderRadius: "0.75rem", marginTop: "0.75rem" }}
-          src={`social-media-clone-api-three.vercel.app/assets/${picturePath}`}
+          src={`https://social-media-clone-api-three.vercel.app/assets/${picturePath}`}
         />
       )}
       <FlexBetween mt="0.25rem">

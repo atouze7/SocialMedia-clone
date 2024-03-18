ffa5b37fb1682df572bb21363482e2f1b2d042f2
atouze7 <89221133+atouze7@users.noreply.github.com>
2024-03-18 14:15:49 -0400

    Update FriendListWidget.jsx

--- client/src/scenes/widgets/FriendListWidget.jsx
+++ client/src/scenes/widgets/FriendListWidget.jsx
@@ -13,7 +13,7 @@ const FriendListWidget = ({ userId }) => {
 
   const getFriends = async () => {
     const response = await fetch(
-      `social-media-clone-api-three.vercel.app/users/${userId}/friends`,
+      `https://social-media-clone-api-three.vercel.app/users/${userId}/friends`,
       {
         method: "GET",
         headers: { Authorization: `Bearer ${token}` },

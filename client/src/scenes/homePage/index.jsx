239b6822c1749131d3bb7381da81383315fe3a3b
atouze7 <atouze7@gmail.com>
2024-03-18 11:50:44 -0400

    Initial Commit

---
+++ client/src/scenes/homePage/index.jsx
@@ -0,0 +1,43 @@
+import { Box, useMediaQuery } from "@mui/material";
+import React from "react";
+import { useSelector } from "react-redux";
+import Navbar from "scenes/navbar";
+import UserWidget from "scenes/widgets/UserWidget";
+import MyPostWidget from "scenes/widgets/MyPostWidget";
+import PostsWidget from "scenes/widgets/PostsWidget";
+import AdvertWidget from "../widgets/AdvertWidget";
+
+const HomePage = () => {
+  const isNonMobile = useMediaQuery("(min-width: 1000px");
+  const { _id, picturePath } = useSelector((state) => state.user);
+  return (
+    <Box>
+      <Navbar />
+      <Box
+        width="100%"
+        padding="2rem 6%"
+        display={isNonMobile ? "flex" : "block"}
+        gap="0.5rem"
+        justifyContent="space-between"
+      >
+        <Box flexBasis={isNonMobile ? "26%" : undefined}>
+          <UserWidget userId={_id} picturePath={picturePath} />
+        </Box>
+        <Box
+          flexBasis={isNonMobile ? "42%" : undefined}
+          mt={isNonMobile ? undefined : "2rem"}
+        >
+          <MyPostWidget picturePath={picturePath} />
+          <PostsWidget userId={_id} />
+        </Box>
+        {isNonMobile && (
+          <Box flexBasis="26%">
+            <AdvertWidget />
+          </Box>
+        )}
+      </Box>
+    </Box>
+  );
+};
+
+export default HomePage;

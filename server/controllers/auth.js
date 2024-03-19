6fea0b2aca83f690b51b54400f53177284a0b05a
atouze7 <89221133+atouze7@users.noreply.github.com>
2024-03-18 20:56:21 -0400

    Update auth.js

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -2,7 +2,42 @@ import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
 
-//register user
+export const register = async (req, res) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      location,
+      occupation,
+    } = req.body;
+
+    // Assuming the picture is sent as a file and handled by multer middleware
+    const picturePath = req.file ? req.file.path : ''; // Check if picture is uploaded
+
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+      picturePath,
+      location,
+      occupation,
+      viewedProfile: Math.floor(Math.random() * 10000),
+      impressions: Math.floor(Math.random() * 10000),
+    });
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+/*register user
 export const register = async (req, res) => {
   try {
     const {
@@ -38,7 +73,7 @@ export const register = async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
-};
+};*/
 
 export const login = async (req, res) => {
   try {
